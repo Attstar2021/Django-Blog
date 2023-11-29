0561c1e16404b48c7c9ee5184621f1653cc17ed9
Attstar2021 <>
2023-11-29 03:31:39 +0000

    update link for Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-attstar2021-django-blog-bgd356gc5y.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['codestar-attstat-46f75ee3a8a4.herokuapp.com', '8000-attstar2021-django-blog-bgd356gc5y.us2.codeanyapp.com']
 
 
 # Application definition
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
